3e4950dd1c78da5766ebbe67ba7d6dd854c378f8
Antoine Krull <a.krull@campus.lmu.de>
2022-11-22 18:19:23 +0100

    added attribtes and methods to the board class; changed the type to abstract

--- src/main/java/game/board/Board.java
+++ src/main/java/game/board/Board.java
@@ -1,4 +1,10 @@
 package game.board;
 
 public abstract class Board {
+    protected int columns;
+    protected int rows;
+    protected Tile[][] board;
+
+    public void loadBoard(){}
+    public void setTile(int colum, int row, Tile tile){}
 }
