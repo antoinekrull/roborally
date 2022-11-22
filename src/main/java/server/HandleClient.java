10cdb6e7ca4e689f59a7c1f365736d370e4d749a
Antoine Krull <a.krull@campus.lmu.de>
2022-11-22 09:36:56 +0100

    added handleclient logic

--- src/main/java/server/HandleClient.java
+++ src/main/java/server/HandleClient.java
@@ -1,9 +1,225 @@
 package server;
 
+import communication.JsonSerializer;
+import communication.Message;
+import communication.MessageType;
+import game.player.Player;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+
 public class HandleClient implements Runnable{
 //TODO: HandleClient class
-    @Override
-    public void run() {
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+    public String address;
+    public int port;
+    public Socket socket;
+    private String username;
+    private ServerMain.Server server;
 
+    /**
+     * Thread which handles the loged in clients.
+     *
+     * @param address
+     * @param port
+     * @param socket
+     */
+    public HandleClient(String address, int port, Socket socket, ServerMain.Server server) {
+        this.address = address;
+        this.port = port;
+        this.socket = socket;
+        this.server = server;
+        try {
+            this.in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+            this.out = new DataOutputStream(socket.getOutputStream());
+        } catch (Exception e) {
+            System.out.println("Error here " + e.getMessage());
+        }
+    }
+
+    public boolean containsName(final List<HandleClient> list, final String name) {
+        if (list.size() > 0) {
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).getUsername().equals(name)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void write(Message message) {
+        try {
+            this.out.writeUTF(JsonSerializer.serializeJson(message));
+        } catch (IOException e) {
+            System.out.println("Error here " + e.getMessage());
+        }
+    }
+
+    /**
+     * Server can write to a specific client.
+     *
+     * @param username The targeted client.
+     * @param message  Message the server send to the client.
+     */
+    //TODO: Prints multiple times, can sometimes crash threads
+    public void writeTo(String username, Message message) {
+        try {
+            for (server.HandleClient client : server.CLIENTS) {
+                if (client.getUsername().equals(username)) {
+                    client.out.writeUTF(JsonSerializer.serializeJson(message));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Send info to client that he is connected to server.
+     *
+     * @param username Client who is connecting.
+     */
+    public void grantAccess(String username) {
+        setUsername(username);
+        Message access = new Message();
+        access.setMessageType(MessageType.USERNAME_COMMAND);
+        access.setUsername("Server");
+        access.setMessage("accepted");
+        writeTo(username, access);
+    }
+
+    /**
+     * Send info to client when username is already taken.
+     *
+     * @param username Client who tries to log in.
+     */
+    public void denyAccess(String username) {
+        setUsername(username);
+        Message access = new Message();
+        access.setMessageType(MessageType.USERNAME_COMMAND);
+        access.setUsername("Server");
+        access.setMessage("The username " + username + " is already taken, choose another one");
+        writeTo(username, access);
+        setUsername(null);
+    }
+
+    @Override
+    public String toString() {
+        return getUsername();
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    /**
+     * Runs the server and game logic.
+     */
+    public void run() {
+        try {
+            String username = null;
+
+            while (username == null) {
+                username = JsonSerializer.deserializeJson(in.readUTF(), Message.class).getUsername();
+                if (!containsName(server.CLIENTS, username)) {
+                    grantAccess(username);
+                } else {
+                    denyAccess(username);
+                    username = null;
+                }
+            }
+            if (this.username.isBlank()) {
+                //TODO: Review this part of the code
+                out.writeUTF(JsonSerializer.serializeJson(new Message("", "Connection Accepted"
+                        + "\nWelcome " + username + "\nNOTE: SINCE YOU HAVEN'T PROVIDED AN USERNAME, " +
+                        "YOU'RE IN MODE READ ONLY.")));
+            } else {
+                // Thread needs to sleep so that the chat form can load and the user sees his welcome message
+                Thread.sleep(1000);
+                //welcome message to server
+                String message = "Welcome " + this.username;
+                try {
+                    server.messages.put(message);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            String line = "";
+            while (!line.equals("bye")) {
+                Message incomingMessage = JsonSerializer.deserializeJson(this.in.readUTF(), Message.class);
+                try {
+                    if (incomingMessage.getMessageType() == MessageType.GROUP_CHAT) {
+                        line = incomingMessage.getMessage();
+                        String line_formatted = this.username + ":  " + line;
+
+                        if (!this.username.isBlank()) {
+                            server.messages.put(line_formatted);
+                            //print to server
+                        }
+                    } else if (incomingMessage.getMessageType() == MessageType.DIRECT_MESSAGE) {
+                        if (containsName(server.CLIENTS, incomingMessage.getTarget())) {
+                            writeTo(incomingMessage.getTarget(), incomingMessage);
+                        } else {
+                            writeTo(incomingMessage.getUsername(), new Message("Server",
+                                    "Invalid username, try again"));
+                        }
+                    } else if (incomingMessage.getMessageType() == MessageType.JOIN_SESSION) {
+                        if (server.players.playerIsInList(incomingMessage.getUsername())) {
+                            server.messages.put(incomingMessage.getUsername() + " already in gamesession");
+                        } else {
+                            server.players.add(new Player(incomingMessage.getUsername(), server));
+                            server.messages.put(incomingMessage.getUsername() + " joined the gamesession");
+                        }
+                    } else if (incomingMessage.getMessageType() == MessageType.LEAVE_SESSION) {
+                        server.players.remove(server.players.getPlayerFromList(incomingMessage.getUsername()).getUsername());
+                        if (server.players.playerIsInList(incomingMessage.getUsername())) {
+                            server.activePlayers.remove(server.players.getPlayerFromList(incomingMessage.getUsername()).getUsername());
+                            if (server.activePlayers.size() == 1) {
+                                writeTo(server.activePlayers.get(0).getUsername(), new Message("Server",
+                                        "All other players left, please restart"));
+                            }
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println(this.username);
+                }
+            }
+
+            try {
+                String goodbyeMessage = "Server:  Hasta la vista " + this.username;
+                //may not work since player not only identified by his username
+                server.players.remove(username);
+                server.messages.put(goodbyeMessage);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            server.CLIENTS.remove(this);
+            this.in.close();
+            this.out.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("Error :" + e.getMessage());
+        }
     }
 }
