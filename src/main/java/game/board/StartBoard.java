a6bab2f413b45be6951b5d2dde8fb7717c9e224c
Antoine Krull <a.krull@campus.lmu.de>
2022-11-23 09:59:32 +0100

    added StartBoard

--- src/main/java/game/board/StartBoard.java
+++ src/main/java/game/board/StartBoard.java
@@ -1,2 +1,14 @@
-package game.board;public class StartBoard {
+package game.board;
+
+/**
+ * @author Antoine
+ * @version 1.0
+ */
+public class StartBoard extends Board{
+
+    public StartBoard(int rows, int columns){
+        this.columns = columns;
+        this.rows = rows;
+        this.board = new Tile[rows][columns];
+    }
 }
