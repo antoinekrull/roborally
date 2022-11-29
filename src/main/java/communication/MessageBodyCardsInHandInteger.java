f96b1a974c918bf449cc0d489f34f9f2afbf078a
Antoine Krull <a.krull@campus.lmu.de>
2022-11-29 18:05:19 +0100

    added child classes to handle ambigous naming of the cardsInHand attribute

--- src/main/java/communication/MessageBodyCardsInHandInteger.java
+++ src/main/java/communication/MessageBodyCardsInHandInteger.java
@@ -1,2 +1,13 @@
-package communication;public class MessageBodyCardsInHandInteger {
+package communication;
+
+public class MessageBodyCardsInHandInteger extends MessageBody{
+    private int cardsInHand;
+
+    public int getCardsInHand() {
+        return cardsInHand;
+    }
+
+    public void setCardsInHand(int cardsInHand) {
+        this.cardsInHand = cardsInHand;
+    }
 }
--- src/main/java/communication/MessageBodyCardsInHandStringArray.java
+++ src/main/java/communication/MessageBodyCardsInHandStringArray.java
@@ -1,2 +1,13 @@
-package communication;public class MessageBodyCardsInHandStringArray {
+package communication;
+
+public class MessageBodyCardsInHandStringArray extends MessageBody{
+    private String[] cardsInHand;
+
+    public String[] getCardsInHand() {
+        return cardsInHand;
+    }
+
+    public void setCardsInHand(String[] cardsInHand) {
+        this.cardsInHand = cardsInHand;
+    }
 }
