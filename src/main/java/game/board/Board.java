79a332f01d68b7a89123b65ec1b71e5a3f4da012
Antoine Krull <a.krull@campus.lmu.de>
2023-01-14 11:36:33 +0100

    changed datatype of antenna field

--- src/main/java/game/board/Board.java
+++ src/main/java/game/board/Board.java
@@ -35,7 +35,7 @@ public class Board {
     public static ArrayList<Tile> robotLaserList = new ArrayList<>();
     public static ArrayList<RebootTile> rebootTileList = new ArrayList<>();
     public static ArrayList<StartTile> startTileList = new ArrayList<>();
-    public static ArrayList<Antenna> antennaTileList = new ArrayList<>();
+    public static Antenna antenna;
 
     public static ArrayList<Tile> getTile(Pair<Integer, Integer> position){
             return board.get(position.getValue0()).get(position.getValue1());
@@ -138,7 +138,7 @@ public class Board {
                                     }
                                     case "Antenna" -> {
                                         replaceTileInMap(board,x,y,tile, new Antenna(x,y));
-                                        antennaTileList.add(new Antenna(x, y));
+                                        antenna = new Antenna(x, y);
                                     }
                                     case "PushPanel" -> {
                                         ArrayList<Integer> registerList = tile.getRegisters();
