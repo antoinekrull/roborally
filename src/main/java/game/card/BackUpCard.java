e84112900ab1526ce6b7840fb0923d3d26189d22
Firas <hamdoun@cip.ifi.lmu.de>
2023-01-15 20:42:33 +0100

    -BackUpCard: Effect was missing, now implemented -SandboxRoutineCard: Implemented card effect, will need a special condition when executing from the register, since the player has to choose the effect to apply

--- src/main/java/game/card/BackUpCard.java
+++ src/main/java/game/card/BackUpCard.java
@@ -1,5 +1,10 @@
 package game.card;
 
+import game.CollisionCalculator;
+import game.board.Direction;
+import game.player.Player;
+import org.javatuples.Pair;
+
 public class BackUpCard extends Card {
     int velocity = -1;
 
@@ -8,11 +13,30 @@ public class BackUpCard extends Card {
         setCardName("Move Back");
     }
 
-    public int getVelocity() {
-        return velocity;
+    @Override
+    public void applyEffect(Player player) throws Exception {
+        Direction originalDirection = player.getRobot().getDirection(); //the original direction is saved
+        //the direction is flipped to allow for collision checking, will be later restored to its original value
+        switch (player.getRobot().getDirection()){
+            case NORTH -> player.getRobot().setDirection(Direction.SOUTH);
+            case SOUTH -> player.getRobot().setDirection(Direction.NORTH);
+            case EAST -> player.getRobot().setDirection(Direction.WEST);
+            case WEST -> player.getRobot().setDirection(Direction.EAST);
+        }
+        //code from MoveCard1
+        Pair<Integer, Integer> newPosition = new Pair<>(player.getRobot().getCurrentPosition().getValue0(),
+                player.getRobot().getCurrentPosition().getValue1());
+        for(int i = 0; i < 1; i++){
+            if(CollisionCalculator.checkRobotCollision(player)){
+                switch(player.getRobot().getDirection()){
+                    case NORTH -> newPosition.setAt0(newPosition.getValue0() + 1);
+                    case SOUTH -> newPosition.setAt0(newPosition.getValue0() - 1);
+                    case EAST -> newPosition.setAt1(newPosition.getValue1() + 1);
+                    case WEST -> newPosition.setAt1(newPosition.getValue1() - 1);
+                }
+            }
+        }
+        player.getRobot().setCurrentPosition(newPosition);
+        player.getRobot().setDirection(originalDirection);
     }
-    public void setVelocity(int velocity) {
-        this.velocity = velocity;
-    }
-
 }
