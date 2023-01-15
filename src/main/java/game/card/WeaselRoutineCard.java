3adc7831988587a87fcde829ff2b4dd50439dee7
Firas <hamdoun@cip.ifi.lmu.de>
2023-01-15 20:23:52 +0100

    Implemented WeaselRoutineCard, will need a special condition when executing from the register, since the player has to choose the effect to apply

--- src/main/java/game/card/WeaselRoutineCard.java
+++ src/main/java/game/card/WeaselRoutineCard.java
@@ -1,5 +1,6 @@
 package game.card;
 
+import game.board.Direction;
 import game.player.Player;
 
 public class WeaselRoutineCard extends Card{
@@ -8,8 +9,35 @@ public class WeaselRoutineCard extends Card{
         setCardName("Weasel Routine");
     }
 
-    @Override
-    public void applyEffect(Player player) throws Exception {
-
+    public void applyEffect(Player player, String cardName) throws Exception {
+        switch (cardName){
+            case "Left Turn" -> {
+                switch (player.getRobot().getDirection()){
+                    case NORTH -> player.getRobot().setDirection(Direction.WEST);
+                    case WEST -> player.getRobot().setDirection(Direction.SOUTH);
+                    case SOUTH -> player.getRobot().setDirection(Direction.EAST);
+                    case EAST -> player.getRobot().setDirection(Direction.NORTH);
+                    default -> throw new Exception("Invalid direction");
+                }
+            }
+            case "Right Turn" -> {
+                switch (player.getRobot().getDirection()){
+                    case NORTH -> player.getRobot().setDirection(Direction.EAST);
+                    case EAST -> player.getRobot().setDirection(Direction.SOUTH);
+                    case SOUTH -> player.getRobot().setDirection(Direction.WEST);
+                    case WEST -> player.getRobot().setDirection(Direction.NORTH);
+                    default -> throw new Exception("Invalid direction");
+                }
+            }
+            case "U-Turn" -> {
+                switch (player.getRobot().getDirection()) {
+                    case NORTH -> player.getRobot().setDirection(Direction.SOUTH);
+                    case EAST -> player.getRobot().setDirection(Direction.WEST);
+                    case SOUTH -> player.getRobot().setDirection(Direction.NORTH);
+                    case WEST -> player.getRobot().setDirection(Direction.EAST);
+                    default -> throw new Exception("Invalid direction");
+                }
+            }
+        }
     }
 }
