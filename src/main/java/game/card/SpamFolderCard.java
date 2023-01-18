3dc4a64894ae40eebcf8eb8a34ec64ced1d2b032
Firas <hamdoun@cip.ifi.lmu.de>
2023-01-18 10:51:40 +0100

    Removed special programming cards

--- src/main/java/game/card/SpamFolderCard.java
+++
@@ -1,8 +0,0 @@
-package game.card;
-
-public class SpamFolderCard extends Card{
-    public SpamFolderCard(){
-        cardType = CardType.SPECIAL_PROGRAMMING_CARD;
-        setCardName("Spam Folder");
-    }
-}
