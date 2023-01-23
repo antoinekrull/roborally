2b55c0d0b490a986aa3d7382456d2b63378a12cd
Antoine Krull <a.krull@campus.lmu.de>
2023-01-23 16:42:18 +0100

    started working on shortest path

--- src/main/java/game/player/AI_Player.java
+++ src/main/java/game/player/AI_Player.java
@@ -1,9 +1,16 @@
 package game.player;
 
+import game.board.Board;
 import game.card.Card;
 import game.card.CardType;
+import helper.Helper;
+import helper.Node;
+import org.javatuples.Pair;
+
+import java.util.ArrayList;
 
 public class AI_Player extends Player {
+    private Board board;
     public AI_Player(int id, String username, Robot robot) {
         super(id, username, robot);
     }
@@ -38,5 +45,18 @@ public class AI_Player extends Player {
             }
         }
     }
+
+//    public ArrayList<Node> getShortestPath(){
+//        Pair<Integer, Integer> currentObjective = Helper.getPositionOfCurrentObjective(robot.getCurrentObjective(), board);
+//
+//    }
+
+    /**
+     * The setter for the board. This is important as KIs are generated before a board exists. But for getting the
+     * shortest path, a board is needed. So after starting the game, a board is given to the AI player.
+     *
+     * @param board The board on wich is played
+     */
+    public void setBoard(Board board){ this.board = board; }
 
 }
