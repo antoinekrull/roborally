23a8c9f770a31522b9f450cec09a08870574559e
Antoine Krull <a.krull@campus.lmu.de>
2023-01-23 16:40:04 +0100

    added helper classes

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -1,2 +1,25 @@
-package helper;public class Helper {
+package helper;
+
+import game.board.Board;
+import game.board.CheckpointTile;
+import org.javatuples.Pair;
+
+public class Helper {
+
+    /**
+     * Private constructor to prevent anyone of making an instance of the helper class.
+     */
+    private Helper(){}
+    public static Pair<Integer, Integer> getPositionOfCurrentObjective(int currentObjective, Board board){
+        for(int x = 0; x < board.getBoard().size(); x++){
+            for(int y = 0; y < board.getBoard().get(x).size(); y++) {
+                if (board.getBoard().get(x).get(y).get(0) instanceof CheckpointTile ct){
+                    if(ct.getNumber() == currentObjective){
+                        return new Pair<>(ct.getXCoordinate(), ct.getYCoordinate());
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/helper/Node.java
+++ src/main/java/helper/Node.java
@@ -1,2 +1,35 @@
-package helper;public class Node {
+package helper;
+
+public class Node implements Comparable<Node>{
+    private final int xCoordinate;
+    private final int yCoordinate;
+    private final int distance;
+    private Node lastNode;
+    public Node(int xCoordinate, int yCoordinate, int distance, Node lastNode){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.distance = distance;
+        this.lastNode = lastNode;
+    }
+
+    @Override
+    public int compareTo(Node node) {
+        return Integer.compare(this.distance, node.distance);
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Node getLastNode() {
+        return lastNode;
+    }
 }
