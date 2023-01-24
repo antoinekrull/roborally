6f7396a39bfda5c0f364046bef31233db1d0a199
Antoine Krull <a.krull@campus.lmu.de>
2023-01-24 10:56:46 +0100

    added comments

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -12,6 +12,14 @@ public class Helper {
      * Private constructor to prevent anyone of making an instance of the helper class.
      */
     private Helper(){}
+
+    /**
+     * Returns the position of the current objective.
+     *
+     * @param currentObjective The current objective of the player
+     * @param board The board on wich is played
+     * @return A pair of integers representing the x and y coordinates of the current objective
+     */
     public static Pair<Integer, Integer> getPositionOfCurrentObjective(int currentObjective, Board board){
         for(int x = 0; x < board.getBoard().size(); x++){
             for(int y = 0; y < board.getBoard().get(x).size(); y++) {
@@ -25,6 +33,13 @@ public class Helper {
         return null;
     }
 
+    /**
+     * Checks if a robot stands on a tile.
+     *
+     * @param tile The selected tile.
+     * @param robot The selected robot.
+     * @return Either true of false depending on the robot standing on the tile or not.
+     */
     public static boolean isRobotOnTile(Robot robot, Tile tile){
         return robot.getCurrentPosition().getValue0() == tile.getXCoordinate() &&
                 robot.getCurrentPosition().getValue1() == tile.getYCoordinate();
