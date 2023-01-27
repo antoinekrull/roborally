78b51258db7ecc8dda13510544e8ab64cd7a7f68
Dominic Manuel Lenz <lenz@cip.ifi.lmu.de>
2023-01-27 16:02:12 +0100

    - added switch case again but this time right

--- src/main/java/game/board/Board.java
+++ src/main/java/game/board/Board.java
@@ -107,10 +107,9 @@ public class Board {
                                 ConveyorBeltTile conveyor = new ConveyorBeltTile(x, y, velocity, in, out);
                                 replaceTileInMap(board, x, y, tile, conveyor);
 
-                                if(velocity==1) {
-                                    conveyorBelt1List.add(conveyor);
-                                } else if(velocity==2){
-                                    conveyorBelt2List.add(conveyor);
+                                switch (velocity) {
+                                    case 1 -> conveyorBelt1List.add(conveyor);
+                                    case 2 -> conveyorBelt2List.add(conveyor);
                                 }
                             }
                             case "Wall" -> {
