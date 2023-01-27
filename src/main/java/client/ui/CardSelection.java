f0e3cf86c3a02c94149f2a3351e3a1b242b94489
Benedikt Weinheimer <benedikt.weinheimer@gmail.com>
2023-01-27 17:02:36 +0100

    prepared Overlay for pickDamage

--- src/main/java/client/ui/CardSelection.java
+++ src/main/java/client/ui/CardSelection.java
@@ -2,10 +2,12 @@ package client.ui;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.StackPane;
 
 public class CardSelection {
@@ -19,35 +21,62 @@ public class CardSelection {
     baseStackPane.getChildren().add(overlay);
     Label label = new Label("Cards to pick: " + counter);
     overlay.getChildren().add(label);
+    List<ImageView> imageViews = new ArrayList<>();
     for (String card : damagePiles) {
-      ImageView damagepile = new ImageView();
       switch (card) {
         case "Worm" -> {
           InputStream input1 = getClass().getResourceAsStream("/textures/cards/WORM.png");
-          damagepile.setImage(new Image(input1));
-          damagepile.setPreserveRatio(true);
-          damagepile.setFitWidth(200);
-          overlay.getChildren().add(damagepile);
+          Image image1 = new Image(input1);
+          ImageView imageView1 = new ImageView(image1);
+          imageView1.setId("Spam");
+          imageView1.setPreserveRatio(true);
+          imageView1.setFitWidth(200);
+          overlay.getChildren().add(imageView1);
+          imageViews.add(imageView1);
         }
         case "Virus" -> {
           InputStream input2 = getClass().getResourceAsStream(
               "/textures/cards/VIRUS.png");
-          damagepile.setImage(new Image(input2));
-          damagepile.setPreserveRatio(true);
-          damagepile.setFitWidth(200);
-          overlay.getChildren().add(damagepile);
+          Image image2 = new Image(input2);
+          ImageView imageView2 = new ImageView(image2);
+          imageView2.setId("Worm");
+          imageView2.setImage(new Image(input2));
+          imageView2.setPreserveRatio(true);
+          imageView2.setFitWidth(200);
+          overlay.getChildren().add(imageView2);
+          imageViews.add(imageView2);
         }
         case "Tojan" -> {
           InputStream input3 = getClass().getResourceAsStream(
               "/textures/cards/TROJAN_HORSE.png");
-          damagepile.setImage(new Image(input3));
-          damagepile.setPreserveRatio(true);
-          damagepile.setFitWidth(200);
-          overlay.getChildren().add(damagepile);
+          Image image3 = new Image(input3);
+          ImageView imageView3 = new ImageView(image3);
+          imageView3.setId("Virus");
+          imageView3.setImage(new Image(input3));
+          imageView3.setPreserveRatio(true);
+          imageView3.setFitWidth(200);
+          overlay.getChildren().add(imageView3);
+          imageViews.add(imageView3);
         }
       }
+    }
 
+    for (ImageView imageView : imageViews) {
+      imageView.setOnMouseClicked(event -> {
+        if (event.getButton() == MouseButton.PRIMARY) {
+          //TODO: send Message
+          int finalCounter = counter;
+          finalCounter--;
+          if (finalCounter == 0) {
+            baseStackPane.getChildren().remove(overlay);
+            for (String c : damagePiles) {
+              overlay.getChildren().remove(c);
+            }
+          }
+        }
+      });
+    }
     }
   }
 
-}
+
