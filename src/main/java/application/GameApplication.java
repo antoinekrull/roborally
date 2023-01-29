01fd961dca6102a3ac2abbd7891e8ee8cbeaef74
Benedikt Weinheimer <benedikt.weinheimer@gmail.com>
2023-01-30 00:46:58 +0100

    FATjar preparation

---
+++ src/main/java/application/GameApplication.java
@@ -0,0 +1,11 @@
+package application;
+
+public class GameApplication {
+
+  //For FATjar Creation
+
+  public static void main(String[] args){
+    client.RoboRallyStart.main(args);
+  }
+
+}
---
+++ src/main/java/application/GameServer.java
@@ -0,0 +1,9 @@
+package application;
+
+public class GameServer {
+//For FATjar Creation
+
+  public static void main(String[] args){
+    server.ServerStart.main(args);
+  }
+}
